f65a36cf362de1426237f1d0a7d60968b4048934
FMW14 <fmw14@yandex.ru>
2020-07-06 18:17:32 +0300

    logback added

--- src/main/java/com/example/springDemoHW/controllers/MainController.java
+++ src/main/java/com/example/springDemoHW/controllers/MainController.java
@@ -1,5 +1,7 @@
 package com.example.springDemoHW.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +10,34 @@ import java.time.ZonedDateTime;
 
 @Controller
 public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
     @GetMapping("/")
-    public String greeting(Model model) {
-        model.addAttribute("time", ZonedDateTime.now());
+    public String root(Model model) {
+        logger.info("GET request received for \"/\"");
+
+        try {
+            model.addAttribute("time", ZonedDateTime.now());
+        } catch (RuntimeException e){
+            logger.error("RuntimeException in root", e);
+        }
+
         return "main";
+    }
+
+    @GetMapping("/err")
+    public String err(Model model) {
+        logger.info("GET request received for \"/err\"");
+
+        try {
+            simulate();
+        } catch (NullPointerException e) {
+            logger.error("npm test", e);
+        }
+
+        return "err";
+    }
+
+    public int simulate() throws NullPointerException{
+        throw new NullPointerException("NPM EXCEPTION TEST");
     }
 }
